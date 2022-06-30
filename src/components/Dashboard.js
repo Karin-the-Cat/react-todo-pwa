c541ce26730c2a566a97ee17114662e6218b54dc
Miyu Watanabe <mw127.fbs@gmail.com>
2022-06-30 15:44:00 +0900

    first commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,82 @@
+import React, {useState, useEffect, useContext} from "react";
+import * as Api from "../service/api"
+import { TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { signInWithGoogle, logOut } from "../service/firebase";
+import dig from "object-dig";
+import { AuthContext } from "../provider/AuthProvider";
+import ToDoList from "./ToDoList"
+
+const useStyles = makeStyles(() => ({
+  root: {
+    textAlign: 'center',
+    marginTop: 40,
+    justifyContent: 'space-between',
+  },
+  form: {
+    width:"100%",
+    maxWidth:360,
+    margin: "auto",
+    marginBottom: 40,
+    display:"flex",
+    alignItems:"baseline",
+    justifyContent:"center"
+  },
+  input: {
+    marginRight: 10,
+  }
+}));
+
+const Dashboard = () => {
+  const classes = useStyles();
+  const currentUser = useContext(AuthContext);
+  const [inputName, setInputName ] = useState("");
+  const [todos, setTodos] = useState([]);
+  console.log(inputName);
+
+  useEffect(() => {
+    // TODO一覧を取得
+    fetch();
+  }, [currentUser])
+
+  const fetch = async() => {
+    if( dig(currentUser, 'currentUser','uid')){
+      const data = await Api.initGet(currentUser.currentUser.uid);
+      setTodos(data);
+      console.log(todos);
+    }
+  }
+
+  const formRender = () => {
+    let dom
+    // ログイン時
+    if(dig(currentUser,'currentUser','uid')){
+      dom = <form className={classes.form}>
+        <TextField placeholder="ToDoName" className={classes.input} value={inputName} onChange={(event) => setInputName(event.currentTarget.value)} />
+        <Button variant="inherit" type="button" color="primary" size="small" disabled={inputName.length > 0 ? false : true} onClick={() => post()}>追加</Button>
+        
+      </form>  
+    }else{
+      dom = <button onClick={signInWithGoogle}>ログイン</button>
+    }
+    return dom;
+  }
+
+  const post = async() => {
+    await Api.addTodo(inputName, currentUser.currentUser.uid);
+    await setInputName("");
+    fetch();
+
+  }
+
+
+  return (
+    <div className={classes.root}>
+      {formRender()}
+      <ToDoList todos={todos} fetch={fetch}/>
+    </div>
+  );
+}
+
+export default Dashboard;
